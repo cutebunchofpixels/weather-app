9bb88da7758be3b4c5b23280250a0aecec67d2a8
Anton Poroshkin <poroshkinanton18@gmail.com>
2024-02-03 16:22:23 +0200

    feat: document direction switch setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,55 @@
-import { Suspense } from "react";
+import { Suspense, useMemo } from "react";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
 import Home from "./components/pages/home/Home";
+import { CssBaseline } from "@mui/material";
+import { ThemeProvider } from "@mui/material/styles";
+import { prefixer } from "stylis";
+import createCache from "@emotion/cache";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import { defaultTheme } from "./utils/theme";
+
+const rtlCache = createCache({
+    key: "muirtl",
+    stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const ltrCache = createCache({
+    key: "muiltr",
+});
 
 function App() {
+    const { i18n } = useTranslation();
+
+    useEffect(() => {
+        document.dir = i18n.dir() || "ltr";
+    });
+
+    const theme = useMemo(() => {
+        const theme = { ...defaultTheme, direction: i18n.dir() };
+        return theme;
+    }, []);
+
     return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <Box
-                sx={{ display: "flex", minHeight: "100vh", padding: "1.5rem" }}
-            >
-                <Home />
-            </Box>
-        </Suspense>
+        <CacheProvider value={i18n.dir() === "ltr" ? ltrCache : rtlCache}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <Box
+                        sx={{
+                            display: "flex",
+                            minHeight: "100vh",
+                            padding: "1.5rem",
+                        }}
+                    >
+                        <Home />
+                    </Box>
+                </Suspense>
+            </ThemeProvider>
+        </CacheProvider>
     );
 }
-
 export default App;
 
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./i18n";
-import { CssBaseline } from "@mui/material";
-import { ThemeProvider } from "@mui/material/styles";
-import { defaultTheme } from "./utils/theme";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <ThemeProvider theme={defaultTheme}>
-            <CssBaseline />
-            <App />
-        </ThemeProvider>
+        <App />
     </React.StrictMode>
 );
 
